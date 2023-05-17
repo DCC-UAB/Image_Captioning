3f2e18f81d86f08df00545669b07738ac3a3a63b
Aledonairesa <aledonairesa@gmail.com>
2023-05-17 13:10:44 +0200

    Continued with understanding

--- utils/utils.py
+++ utils/utils.py
@@ -1,9 +1,13 @@
 import wandb
+import pandas as pd
+from collections import Counter
+import spacy
 import torch
 import torch.nn 
 import torchvision
 import torchvision.transforms as transforms
-from models.models import *
+from torch.utils.data import Dataset, DataLoader
+from models.models import *    
 
 def get_data(slice=1, train=True):
     full_dataset = torchvision.datasets.MNIST(root=".",
@@ -17,11 +21,11 @@ def get_data(slice=1, train=True):
     return sub_dataset
 
 
-def make_loader(dataset, batch_size):
+def make_loader(dataset, batch_size, num_workers=2, shuffle=True):
     loader = torch.utils.data.DataLoader(dataset=dataset,
                                          batch_size=batch_size, 
-                                         shuffle=True,
-                                         pin_memory=True, num_workers=2)
+                                         shuffle=shuffle,
+                                         pin_memory=True, num_workers=num_workers)
     return loader
 
 
@@ -40,3 +44,79 @@ def make(config, device="cuda"):
         model.parameters(), lr=config.learning_rate)
     
     return model, train_loader, test_loader, criterion, optimizer
+
+class Vocabulary:
+    def __init__(self,freq_threshold):
+        #setting the pre-reserved tokens int to string tokens
+        self.itos = {0:"<PAD>",1:"<SOS>",2:"<EOS>",3:"<UNK>"}
+        
+        #string to int tokens
+        #its reverse dict self.itos
+        self.stoi = {v:k for k,v in self.itos.items()}
+        
+        self.freq_threshold = freq_threshold
+        
+    def __len__(self): return len(self.itos)
+    
+    @staticmethod
+    def tokenize(text):
+        spacy_eng = spacy.load("en_core_web_sm")
+        return [token.text.lower() for token in spacy_eng.tokenizer(text)]
+    
+    def build_vocab(self, sentence_list):
+        frequencies = Counter()
+        idx = 4
+        
+        for sentence in sentence_list:
+            for word in self.tokenize(sentence):
+                frequencies[word] += 1
+                
+                #add the word to the vocab if it reaches minum frequecy threshold
+                if frequencies[word] == self.freq_threshold:
+                    self.stoi[word] = idx
+                    self.itos[idx] = word
+                    idx += 1
+    
+    def numericalize(self,text):
+        """ For each word in the text corresponding index token for that word form the vocab built as list """
+        tokenized_text = self.tokenize(text)
+        return [ self.stoi[token] if token in self.stoi else self.stoi["<UNK>"] for token in tokenized_text ] 
+
+class FlickrDataset(Dataset):
+    """
+    FlickrDataset
+    """
+    def __init__(self,root_dir,captions_file,transform=None,freq_threshold=5):
+        self.root_dir = root_dir
+        self.df = pd.read_csv(captions_file)
+        self.transform = transform
+        
+        #Get image and caption colum from the dataframe
+        self.imgs = self.df["image"]
+        self.captions = self.df["caption"]
+        
+        #Initialize vocabulary and build vocab
+        self.vocab = Vocabulary(freq_threshold)
+        self.vocab.build_vocab(self.captions.tolist())
+        
+    
+    def __len__(self):
+        return len(self.df)
+    
+    def __getitem__(self,idx):
+        caption = self.captions[idx]
+        img_name = self.imgs[idx]
+        img_location = os.path.join(self.root_dir,img_name)
+        img = Image.open(img_location).convert("RGB")
+        
+        #apply the transfromation to the image
+        if self.transform is not None:
+            img = self.transform(img)
+        
+        #numericalize the caption text
+        caption_vec = []
+        caption_vec += [self.vocab.stoi["<SOS>"]]
+        caption_vec += self.vocab.numericalize(caption)
+        caption_vec += [self.vocab.stoi["<EOS>"]]
+        
+        return img, torch.tensor(caption_vec)
