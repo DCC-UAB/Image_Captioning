8743ae960190676ffb24764365d17c65be452be8
pau-ventrod <pauventura123@gmail.com>
2023-05-25 21:09:27 +0200

    Git re-structured, style issues, code cleaned

---
+++ SRC/main.py
@@ -0,0 +1,76 @@
+import os
+import random
+import wandb
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as T
+
+from train import *
+from test import *
+from utils.utils import *
+
+# Global variables
+global global_vocab
+global device
+global DATA_LOCATION
+
+DATA_LOCATION = '../data'
+
+# Ensure deterministic behavior
+torch.backends.cudnn.deterministic = True
+random.seed(hash("setting random seeds") % 2**32 - 1)
+np.random.seed(hash("improves reproducibility") % 2**32 - 1)
+torch.manual_seed(hash("by removing stochasticity") % 2**32 - 1)
+torch.cuda.manual_seed_all(hash("so runs are repeatable") % 2**32 - 1)
+
+# Device configuration
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
+def model_pipeline(cfg: dict):
+    # tell wandb to get started
+    with wandb.init(project="pytorch-demo", config=cfg):
+        # access all HPs through wandb.config, so logging matches execution!
+        config = wandb.config
+
+        # execute only once to create the dataset
+        # generate_and_dump_dataset(config.root_dir, config.captions_file, config.transforms)
+
+        # make the model, data, and optimization problem
+        model, train_loader, test_loader, criterion, optimizer = make(config, device)
+
+        # and use them to train the model
+        train(model, train_loader, criterion, optimizer, config)
+
+        # and test its final performance
+        test(model, test_loader, global_vocab)
+
+    return model
+
+
+if __name__ == "__main__":
+    wandb.login()
+
+    transforms = T.Compose([
+        T.Resize(226),
+        T.RandomCrop(224),
+        T.ToTensor(),
+        T.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+    ])
+
+    config = dict(
+        root_dir=DATA_LOCATION+"/Images",
+        captions_file=DATA_LOCATION+"/captions.txt",
+        transforms=transforms,
+        embed_size=300,
+        attention_dim=256,
+        encoder_dim=2048,
+        decoder_dim=512,
+        epochs=25,
+        learning_rate=3e-4,
+        batch_size=256)
+
+    model = model_pipeline(config)
---
+++ SRC/train.py
@@ -0,0 +1,55 @@
+from tqdm.auto import tqdm
+import wandb
+from utils.utils import *
+
+
+def train(model, data_loader, criterion, optimizer, config, num_epochs=25):
+    # Tell wandb to watch what the model gets up to: gradients, weights, and more!
+    wandb.watch(model, criterion, log="all", log_freq=10)
+
+    # Run training and track with wandb
+    total_batches = len(data_loader) * config.epochs
+    example_ct = 0  # number of examples seen
+    batch_ct = 0
+
+    for epoch in tqdm(range(1, num_epochs + 1)):
+        for idx, (image, captions) in enumerate(iter(data_loader)):
+
+            loss = train_batch(image, captions, model, config.vocab_size, optimizer, criterion)
+            example_ct += len(image)
+            batch_ct += 1
+
+            # Report metrics every 25th batch
+            if ((batch_ct + 1) % 25) == 0:
+                train_log(loss, example_ct, epoch)
+
+        # save the latest model
+        save_model(model, epoch, config)
+
+
+def train_batch(image, captions, model, vocab_size, optimizer, criterion, device='cuda'):
+    image, captions = image.to(device), captions.to(device)
+
+    # Zero the gradients.
+    optimizer.zero_grad()
+
+    # Feed forward
+    outputs, attentions = model(image, captions)
+
+    # Calculate the batch loss.
+    targets = captions[:, 1:]
+    loss = criterion(outputs.view(-1, vocab_size), targets.reshape(-1))
+
+    # Backward pass.
+    loss.backward()
+
+    # Update the parameters in the optimizer.
+    optimizer.step()
+
+    return loss
+
+
+def train_log(loss, example_ct, epoch):
+    # Where the magic happens
+    wandb.log({"epoch": epoch, "loss": loss}, step=example_ct)
+    print(f"Loss after {str(example_ct).zfill(5)} examples: {loss:.3f}")
